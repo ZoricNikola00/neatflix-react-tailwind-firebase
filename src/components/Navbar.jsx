2a26e22c8de0f9fafa5653d6b16cac0d1f387f73
ZoricNikola00 <zoriclfc@gmail.com>
2022-10-27 16:24:18 +0200

    Adding Link for Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <nav className='w-full h-auto absolute top-0 left-0 flex justify-between p-4 z-50'>
         <h1 className='cursor-pointer text-4xl font-bold uppercase text-blue-900'>Neatflix</h1>
         <div className='flex'>
-            <button className='py-2 px-4  text-white text-sm sm:text-lg'>Sign In </button>
-            <button className='py-2 px-4 bg-blue-900 text-sm sm:text-lg text-white rounded-lg'>Sign Up</button>
+            <Link to='/signin'><button className='py-2 px-4  text-white text-sm sm:text-lg'>Sign In</button></Link>
+            <Link to='/signup'><button className='py-2 px-4 bg-blue-900 text-sm sm:text-lg text-white rounded-lg'>Sign Up</button></Link>
         </div>
     </nav>
   )
