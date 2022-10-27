3c8b37b8060a56a82b7ae287f8dfe7dd77830aff
ZoricNikola00 <zoriclfc@gmail.com>
2022-10-27 15:30:52 +0200

    Create pages and initialize React Router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import {Route,Routes} from 'react-router-dom'
+import Home from './pages/Home';
+import Login from './pages/Login';
+import SignUp from './pages/SignUp';
+
 
 function App() {
   return (
     <>
-      
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/login' element={<Login/>}/>
+      <Route path='/signup' element={<SignUp/>}/>
+    </Routes>
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,13 +4,14 @@ import './index.css';
 import App from './App';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { BrowserRouter as Router } from 'react-router-dom';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Router>
     <App />
-  </React.StrictMode>
+  </Router>
 );
 
 
