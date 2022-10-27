de67975e342c90d20e49063c22eb59fefb7e20da
ZoricNikola00 <zoriclfc@gmail.com>
2022-10-27 15:48:41 +0200

    Creating Navbar

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import {Route,Routes} from 'react-router-dom'
 import Home from './pages/Home';
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
+import Navbar from './components/Navbar'
 
 
 function App() {
   return (
     <>
+    <Navbar/>
     <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='/login' element={<Login/>}/>
