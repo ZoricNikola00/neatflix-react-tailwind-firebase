5bcdec33ff347da50046c9706064c95af345d1a1
ZoricNikola00 <zoriclfc@gmail.com>
2022-10-28 00:12:32 +0200

    Minor edit

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -4,8 +4,8 @@ import SavedShows from '../components/SavedShows'
 const Account = () => {
   return (
     <>
-        <div className='w-full text-white relative'>
-            <img className='w-full h-[400px] object-cover' src='https://miro.medium.com/max/1400/1*5lyavS59mazOFnb55Z6znQ.png'/>
+        <div className='w-full text-white relative z-40'>
+            <img className='w-full h-[350px] object-cover' src='https://miro.medium.com/max/1400/1*5lyavS59mazOFnb55Z6znQ.png'/>
             <div className='absolute w-full h-full bg-black/60 top-0 left-0'></div>
             <h1 className='absolute top-[20%] text-3xl md:text-5xl font-bold p-4 md:p-8'>My Shows</h1>
         </div>
