4803f437c2956138c49d0d135df60c026b29a927
ZoricNikola00 <zoriclfc@gmail.com>
2022-10-28 15:03:45 +0200

    Change endpoints

--- src/endpoints.js
+++
@@ -1,8 +0,0 @@
-const myKey='72de8895bb64376912ef844faac64a10'
-export const endpoints={
-    latest:`https://api.themoviedb.org/3/movie/latest?api_key=${myKey}&language=en-US`,
-    trending:` https://api.themoviedb.org/3/movie/now_playing?api_key=${myKey}&language=en-US&page=1`,
-    popular:` https://api.themoviedb.org/3/movie/popular?api_key=${myKey}&language=en-US&page=1`,
-    topRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${myKey}&language=en-US&page=1`,
-    upcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${myKey}&language=en-US&page=1`,
-}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,28 @@
 import React from 'react'
 import Categories from '../components/Categories'
 import Main from '../components/Main'
-import { endpoints } from '../endpoints'
+import { useGlobalContext } from '../context'
 
 const Home = () => {
+    const myKey='72de8895bb64376912ef844faac64a10'
+    const {type}=useGlobalContext()
+    const endpoints={
+   
+       onair:` https://api.themoviedb.org/3/tv/on_the_air?api_key=${myKey}&language=en-US&page=1`,
+       trending:` https://api.themoviedb.org/3/${type}/now_playing?api_key=${myKey}&language=en-US&page=1`,
+       popular:` https://api.themoviedb.org/3/${type}/popular?api_key=${myKey}&language=en-US&page=1`,
+       topRated: `https://api.themoviedb.org/3/${type}/top_rated?api_key=${myKey}&language=en-US&page=1`,
+       upcoming: `https://api.themoviedb.org/3/${type}/upcoming?api_key=${myKey}&language=en-US&page=1`,
+   }
+
   return (
     <>
         <Main/>
-        <Categories category={'UpComing'} endpoint={endpoints.upcoming}/>
+        {type==='tv' && <Categories category={'On Air'} endpoint={endpoints.onair}/>}
+        {type==='movie' && <Categories category={'UpComing'} endpoint={endpoints.upcoming}/>}
         <Categories category={'Popular'} endpoint={endpoints.popular}/>
         <Categories category={'Top Rated'} endpoint={endpoints.topRated}/>
-        <Categories category={'Trending'} endpoint={endpoints.trending}/>
+        {type==='movie' && <Categories category={'Trending'} endpoint={endpoints.trending}/>}
     </>
   )
 }
