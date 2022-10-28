27060823f1dfaea53e47815cf68a0800dbd38980
ZoricNikola00 <zoriclfc@gmail.com>
2022-10-28 15:07:11 +0200

    Minor change

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -20,7 +20,7 @@ const SignUp = () => {
   return (
     <>
       <div className='w-full h-screen'>
-        <img className='hidden sm:block absolute w-full h-full object-cover' src='https://miro.medium.com/max/1400/1*5lyavS59mazOFnb55Z6znQ.png'/>
+        <img className=' absolute w-full h-full object-cover' src='https://miro.medium.com/max/1400/1*5lyavS59mazOFnb55Z6znQ.png'/>
         <div className='bg-black/60 fixed top-0 left-0 w-full h-screen'></div>
         <div className='fixed w-full px-4 py-24 z-40'>
           <div className='max-w-[450px] h-[600px] mx-auto bg-black/75 text-white'>
